f055a5699842a6cb94affcb0820280235d07ad59
7H3LaughingMan <7H3LaughingMan@proton.me>
2026-02-13 11:25:11 -0600

    Sort Exports

--- build/exports.js
+++ build/exports.js
@@ -12,29 +12,23 @@ files.sort((a, b) => {
     const a_parts = a.split("/");
     const b_parts = b.split("/");
 
-    const a_folders = a_parts.slice(1, -1);
-    const a_file = a_parts.at(-1) ?? "";
+    for (let index = 0; index < Math.max(a_parts.length, b_parts.length); index++) {
+        const a_part = a_parts[index];
+        const b_part = b_parts[index];
 
-    const b_folders = b_parts.slice(1, -1);
-    const b_file = b_parts.at(-1) ?? "";
+        if (!a_part && !b_part) break;
 
-    for (let index = 0; index < Math.max(a_folders.length, b_folders.length); index++) {
-        const a_folder = a_folders[index];
-        const b_folder = b_folders[index];
+        if (!a_part || !b_parts) {
+            if (!a_part) return -1;
 
-        if (!a_folder && !b_folder) break;
-
-        if (!a_folder || !b_folder) {
-            if (!a_folder) return -1;
-
-            if (!b_folder) return 1;
+            if (!b_parts) return 1;
         }
 
-        const folder_compare = a_folder.localeCompare(b_folder);
-        if (folder_compare !== 0) return folder_compare;
+        const part_compare = a_part.localeCompare(b_part);
+        if (part_compare !== 0) return part_compare;
     }
 
-    return a_file.localeCompare(b_file);
+    return 0;
 });
 
 files = files.filter((value) => !value.endsWith("_module.d.mts"));
--- eslint.config.js
+++ eslint.config.js
@@ -4,25 +4,37 @@ import prettier from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default defineConfig({ plugins: { prettier, json } }, eslint.configs.recommended, tseslint.configs.recommended, {
-    rules: {
-        "@typescript-eslint/no-empty-object-type": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unsafe-declaration-merging": "off",
-        "@typescript-eslint/no-unsafe-function-type": "off",
-        "@typescript-eslint/no-unused-vars": [
-            "error",
-            {
-                args: "all",
-                argsIgnorePattern: "^_",
-                caughtErrors: "all",
-                caughtErrorsIgnorePattern: "^_",
-                destructuredArrayIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
-                ignoreRestSiblings: true,
-            },
-        ],
-        "@typescript-eslint/no-wrapper-object-types": "off",
-        "no-unused-private-class-members": "off",
+export default defineConfig(
+    { plugins: { prettier, json } },
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        rules: {
+            "prettier/prettier": "error",
+            "@typescript-eslint/no-empty-object-type": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unsafe-declaration-merging": "off",
+            "@typescript-eslint/no-unsafe-function-type": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/no-wrapper-object-types": "off",
+            "no-unused-private-class-members": "off",
+        },
     },
-});
+    {
+        files: ["**/*.json"],
+        ignores: ["package-lock.json"],
+        language: "json/json",
+        rules: json.configs.recommended.rules,
+    },
+);
